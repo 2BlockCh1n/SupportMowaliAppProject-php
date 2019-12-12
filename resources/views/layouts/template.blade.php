0fd8087aded8767c31ef64b0e79bea0164cddd3a
2BlockCh1n <kfjbparis@gmail.com>
2019-12-12 18:28:08 +0100

    first test for template

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>SupportMowaliApp-@yield('title')</title>
+
+    <!-- Bootstrap CSS cdn -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Own bootstrap link -->
+  
+  </head>
+  <body>
+    
+   
+    <header class="p-5 mb-5 bg-dark text-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 bg-white"><img   /></div>
+        </div>
+      </div>
+
+    </header>
+      
+    @yield('contenu')
+
+    <!-------------------------------------------------------------------------------------------------->
+    <!-- BOOTSTRAP JQUERY AND POPPER CDN --------------------------------------------------------------->
+    <!-------------------------------------------------------------------------------------------------->
+
+    <!-- jquery Cdn links -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" 
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+
+    <!-- Popper.js Cdn links -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <!-- bootstrap javascript precompiled Cdn links -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" 
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+
+  </body>
+</html>
