af3af66f9d81fc9605be54125da17bb064f6728f
2BlockCh1n <kfjbparis@gmail.com>
2019-12-13 16:21:21 +0100

    add footer on all pages

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -56,29 +56,34 @@
 
         <div class="container"> 
 
-          <div class="card border-dark mr-lg-5 col-md-4" style="max-width: 18rem;">
-            <div class="card-header">Header</div>
-            <div class="card-body text-dark">
-              <h5 class="card-title"></h5>
-              <p class="card-text">   </p>
+          <div class="row">
+
+            <div class="card text-white bg-dark col-md-3 col-md-offset-1 ">
+              <div class="card-header">Header</div>
+              <div class="card-body">
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+              </div>
             </div>
+
+            <div class="card text-white bg-dark mb-3 col-md-offset-1">
+              <div class="card-header">Header</div>
+              <div class="card-body">
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+              </div>
+            </div>
+
+            <div class="card text-white bg-dark mb-3 col-md-offset-1" >
+              <div class="card-header">Header</div>
+              <div class="card-body">
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+              </div>
+            </div>
+
           </div>
 
-          <div class="card border-dark mr-lg-5 col-md-4" style="max-width: 18rem;">
-            <div class="card-header">Header</div>
-            <div class="card-body text-dark">
-              <h5 class="card-title"></h5>
-              <p class="card-text"> </p>
-            </div>
-          </div>
-
-          <div class="card border-dark mb-3 col-md-4" style="max-width: 18rem;">
-            <div class="card-header">Header</div>
-            <div class="card-body text-dark">
-              <h5 class="card-title"></h5>
-              <p class="card-text"></p>
-            </div>
-          </div>
         </div>  
 
     </footer>
