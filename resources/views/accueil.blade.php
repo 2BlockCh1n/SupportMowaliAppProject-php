c313a174c7653c6b4cfa5435759a4b62634ce510
2BlockCh1n <kfjbparis@gmail.com>
2019-12-13 11:59:43 +0100

    add search button an sigin position

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -4,6 +4,6 @@
     Accueil
 @endsection
 
-@section(signin_session)
+@section('signin_session')
     <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Sign in</button>
 @endsection
