030d94d241c6b7b9df0b61edce907edf57223414
2BlockCh1n <kfjbparis@gmail.com>
2019-12-13 11:46:34 +0100

    add search button an sigin position

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -31,13 +31,15 @@
   <!-- nav bar of my template page -->
     <div class="container">
       <nav class="navbar navbar-light bg-light rounded" >
-        <button type="button" class="btn btn-outline-dark col-md-2">Home</button>
-        <button type="button" class="btn btn-outline-dark col-md-3">Informations</button>
-        <button type="button" class="btn btn-outline-dark col-md-2">Members</button>
+        <div class="btn-group-sm">
+          <button type="button" class="btn btn-outline-dark col-md-1">Home</button>
+          <button type="button" class="btn btn-outline-dark col-md-2">Informations</button>
+          <button type="button" class="btn btn-outline-dark col-md-1">Members</button>
+        </div>
         @yield('signin_session')
-        <form class="form-inline my-2 my-lg-0" style="margin-right: 2px;">
+        <form class="form-inline my-2 my-lg-0" style="margin-right: 0px;">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0 col-md-2" type="submit">Search</button>
+          <button class="btn btn-outline-success my-2 my-sm-0 col-md-1" type="submit">Search</button>
         </form>
       <nav>
     </div>
