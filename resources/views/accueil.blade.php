c89b8577ed62c553e32172d20a36f5e61c0c277c
2BlockCh1n <kfjbparis@gmail.com>
2019-12-13 18:19:47 +0100

    add footer on all pages

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -9,7 +9,49 @@
 @endsection
 
 @section('content')
-    <div class="container bg-light shadow-lg p-3 mb-5 bg-white rounded" style="height: 1000px;">
+    <div class="container bg-light shadow-lg p-3 mb-5 bg-white rounded">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{ URL::asset('assets/images/image7.png')}}" class="d-block w-100" alt="...">     
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image1.png')}}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image2.png')}}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image3.png')}}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image4.png')}}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image5.png')}}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image6.png')}}" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
 
 
     </div>
