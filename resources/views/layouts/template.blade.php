ba93d304bb43bce85e6fd378c71652b196884561
2BlockCh1n <kfjbparis@gmail.com>
2019-12-13 12:40:26 +0100

    add search button an sigin position

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -38,12 +38,11 @@
         </div>
       
       <div class="form-group" style="margin-right: 0px;">
-
         <form class="form-inline">
-          <input class=" col-md-offset-2 form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
-        @yield('signin_session')
+          @yield('signin_session')
       </div>
       <nav>
     </div>
