58fce681cfd51c9773f90fe5b3b169477977bea2
2BlockCh1n <kfjbparis@gmail.com>
2019-12-14 11:25:06 +0000

    content review about carousel

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -9,60 +9,62 @@
 @endsection
 
 @section('content')
-    <!-- Creation du div qui englobera tout le carousel -->
-    <div id="option" class="carousel slide" data-ride="carousel">
+    <div class="container">
+        <!-- Creation du div qui englobera tout le carousel -->
+        <div id="option" class="carousel slide" data-ride="carousel">
 
-        <!-- Ajout des indicateurs de pagination en bas de page -->
-        <ul class="carousel-indicators">
-            <li data-target="#option" data-slide-to="0" class="active"></li>
-            <li data-target="#option" data-slide-to="1"></li>
-            <li data-target="#option" data-slide-to="2"></li>
-            <li data-target="#option" data-slide-to="3"></li>
-            <li data-target="#option" data-slide-to="4"></li>
-            <li data-target="#option" data-slide-to="5"></li>
-            <li data-target="#option" data-slide-to="6"></li>    
-        </ul>
+            <!-- Ajout des indicateurs de pagination en bas de page -->
+            <ul class="carousel-indicators">
+                <li data-target="#option" data-slide-to="0" class="active"></li>
+                <li data-target="#option" data-slide-to="1"></li>
+                <li data-target="#option" data-slide-to="2"></li>
+                <li data-target="#option" data-slide-to="3"></li>
+                <li data-target="#option" data-slide-to="4"></li>
+                <li data-target="#option" data-slide-to="5"></li>
+                <li data-target="#option" data-slide-to="6"></li>    
+            </ul>
 
-        <!-- Mise en place du daporama d'image -->
-        <div class="carousel-iner">
-            <!-- On met en place les éléments du diaporama -->
-            <div class="carousel-item active">
-                <img src="{{ URL::asset('assets/images/image7.PNG')}}" alt="mowali-images"/>
+            <!-- Mise en place du daporama d'image -->
+            <div class="carousel-iner">
+                <!-- On met en place les éléments du diaporama -->
+                <div class="carousel-item active">
+                    <img src="{{ URL::asset('assets/images/image7.PNG')}}" alt="mowali-images"/>
+                </div>
+
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image1.PNG')}}" alt="mowali-images"/>
+                </div>
+
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image2.PNG')}}" alt="mowali-images"/>
+                </div>
+
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image3.PNG')}}" alt="mowali-images"/>
+                </div>
+
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image4.PNG')}}" alt="mowali-images"/>
+                </div>
+                
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image5.PNG')}}" alt="mowali-images"/>
+                </div>
+
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('assets/images/image6.PNG')}}" alt="mowali-images"/>
+                </div>
+
+                <!-- On met en place les controle précédent / suivant-->
+                <a href="#option" class="carousel-control-prev" data-slide="prev">
+                    <!-- on met en place l'icone-->
+                    <span class="carousel-control-prev-icon"><span>
+                </a>
+
+                <a href="#option" class="carousel-control-next" data-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                </a>
             </div>
-
-            <div class="carousel-item">
-                <img src="{{ URL::asset('assets/images/image1.PNG')}}" alt="mowali-images"/>
-            </div>
-
-            <div class="carousel-item">
-                <img src="{{ URL::asset('assets/images/image2.PNG')}}" alt="mowali-images"/>
-            </div>
-
-            <div class="carousel-item">
-                <img src="{{ URL::asset('assets/images/image3.PNG')}}" alt="mowali-images"/>
-            </div>
-
-            <div class="carousel-item">
-                <img src="{{ URL::asset('assets/images/image4.PNG')}}" alt="mowali-images"/>
-            </div>
-            
-            <div class="carousel-item">
-                <img src="{{ URL::asset('assets/images/image5.PNG')}}" alt="mowali-images"/>
-            </div>
-
-            <div class="carousel-item">
-                <img src="{{ URL::asset('assets/images/image6.PNG')}}" alt="mowali-images"/>
-            </div>
-
-            <!-- On met en place les controle précédent / suivant-->
-            <a href="#option" class="carousel-control-prev" data-slide="prev">
-                <!-- on met en place l'icone-->
-                <span class="carousel-control-prev-icon"><span>
-            </a>
-
-            <a href="#option" class="carousel-control-next" data-slide="next">
-                <span class="carousel-control-next-icon"></span>
-            </a>
         </div>
     </div>
 @endsection
