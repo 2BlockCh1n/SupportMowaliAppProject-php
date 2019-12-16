478ee451ce8485e8da178273cfdfba6cbc58ea55
2BlockCh1n <kfjbparis@gmail.com>
2019-12-16 13:19:01 +0100

    vue controller added

--- resources/views/signinForm.blade.php
+++ resources/views/signinForm.blade.php
@@ -6,18 +6,18 @@
 
 @section('content')
     
-    <div class="container bg-info rounded" style="height: 300px; padding-top: 100px; padding-left: 50px; padding-right: 50px;">
+    <div class="container bg-info rounded form-group" style="height: 300px; padding-top: 100px; padding-left: 50px; padding-right: 50px;">
         <form action="authenticationSuccess" method="POST" >
             <div>
                 <h2>Sign In</h2> <hr/>
             </div>
 
-            <div class="form-group">
+            <div>
                 <label for="username">Username</label>
                 <input type="text" class="form-control" id="username" placeholder="your username">
             </div><hr/><hr/>
 
-            <div class="form-group">
+            <div>
                 <label for="password">Password</label>
                 <input type="password" class="form-control" id="password" placeholder="your password">
             </div><hr/><hr/>
