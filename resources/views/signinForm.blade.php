3a2df0135e58e80624524bc2117d0251fb97490e
2BlockCh1n <kfjbparis@gmail.com>
2019-12-16 13:14:28 +0100

    vue controller added

--- resources/views/signinForm.blade.php
+++ resources/views/signinForm.blade.php
@@ -5,24 +5,25 @@
 @endsection
 
 @section('content')
- 
-    <form class="container bg-info rounded" action="authenticationSuccess" method="POST" style="height: 300px; padding-top: 100px; padding-left: 50px; padding-right: 50px;">
-        <div>
-            <h2>Sign In</h2> <hr/>
-        </div>
+    
+    <div class="container bg-info rounded" style="height: 300px; padding-top: 100px; padding-left: 50px; padding-right: 50px;">
+        <form action="authenticationSuccess" method="POST" >
+            <div>
+                <h2>Sign In</h2> <hr/>
+            </div>
 
-        <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" class="form-control" id="username" placeholder="your username">
-        </div>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="your username">
+            </div><hr/><hr/>
 
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" placeholder="your password">
-        </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="your password">
+            </div><hr/><hr/>
 
-        <button type="submit" class="btn btn-primary">Submit</button><hr/><hr/>
-    </form>
-
+            <button type="submit" class="btn btn-primary">Submit</button><hr/><hr/>
+        </form>
+    </div>
 
 @endsection
