722fd517c9e96191e202d1a1e79d3e5bf92430f1
2BlockCh1n <kfjbparis@gmail.com>
2019-12-16 14:06:37 +0100

    vue controller added

--- resources/views/signinForm.blade.php
+++ resources/views/signinForm.blade.php
@@ -5,25 +5,28 @@
 @endsection
 
 @section('content')
-    
-    <div class="container bg-info rounded" style="height: 400px; margin-top: 40px;">
-        <form class=" "action="authenticationSuccess" method="POST" >
-                <div>
-                    <h2>Sign In</h2> <hr/>
-                </div>
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="container col-sm-10 bg-info rounded" style="height: 400px; margin-top: 40px;">
+            <form class=" "action="authenticationSuccess" method="POST" >
+                    <div>
+                        <h2>Sign In</h2> <hr/>
+                    </div>
 
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" class="form-control" id="username" placeholder="your username">
-                </div><hr/><hr/>
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" id="username" placeholder="your username">
+                    </div><hr/><hr/>
 
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="your password">
-                </div><hr/><hr/>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="your password">
+                    </div><hr/><hr/>
 
-                <button type="submit" class="btn btn-primary">Submit</button><hr/><hr/>
-        </form>
+                    <button type="submit" class="btn btn-primary">Submit</button><hr/><hr/>
+            </form>
+        </div>
+        <div class="col-sm-1"></div>
     </div>
 
 @endsection
